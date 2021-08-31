5b61bdf2592cb4d068aec958492c5e399e8ce89e
liujie0813 <18220747087@163.com>
2021-09-01 01:12:21 +0800

    temp commit

--- chat-client-web/src/containers/login/index.js
+++ chat-client-web/src/containers/login/index.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react'
-import { Layout } from 'antd'
+import { Layout, Row, Col, Image } from 'antd'
 import './login.css'
+import img1 from '../../common/images/background01.jpg'
 
 const { Header, Content, Footer } = Layout;
 
 class Login extends Component {
 	render() {
 		return (
-			<Layout className="P-layout">
-				<Header className="P-header">Header</Header>
-				<Content style={{ padding: '20px 50px 0', width: '100%' }}>
-					<div className="P-content">Content</div>
+			<Layout style={{ height: '100vh' }}>
+				<Header className="P-L-header">
+					<div style={{ margin: '24px', font: 'italic 1.2em Georgia, serif' }}>
+						TChat
+					</div>
+				</Header>
+				<Content style={{ margin: '30px 50px', backgroundColor: '#fff' }}>
+					<Row style={{ margin: '100px 100px', backgroundColor: '#4169E1' }}>
+						<Col span={12}>
+							<Image src={img1}/>
+						</Col>
+						<Col span={12}>
+							<div>表单</div>
+						</Col>
+					</Row>
 				</Content>
-				<Footer style={{ textAlign: 'center' }}>TChat ©2021 Created by TimberLiu</Footer>
 			</Layout>
 		)
 	}
