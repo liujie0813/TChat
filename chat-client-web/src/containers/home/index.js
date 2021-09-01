f22446c365bdcefca8cf046149f6656bcb707c9b
liujie0813 <18220747087@163.com>
2021-09-01 19:35:15 +0800

    feat: temp commit

--- chat-client-web/src/containers/home/index.js
+++ chat-client-web/src/containers/home/index.js
@@ -1,12 +1,62 @@
 import React, { Component } from 'react'
+import {Button, Form, Input, Layout, Row, Col, Image, Menu, Avatar} from 'antd'
 import './home.css'
+import img1 from '../../common/images/head_portrait.png';
+import img2 from '../../common/images/chat_unactive.png';
+import img3 from '../../common/images/chat_active.png';
+import img4 from '../../common/images/contact_unacitve.png';
+import img5 from '../../common/images/contact_active.png';
+import img6 from '../../common/images/quit.png';
+
+const { Content } = Layout;
 
 class Home extends Component {
+	constructor() {
+		super();
+	}
+
+	handleClick = e => {
+		console.log('click ', e);
+	};
+
 	render() {
 		return (
-			<div className="P-home">
-				<h1>Home page</h1>
-			</div>
+			<Layout style={{ height: '100vh' }}>
+				<Content className='P-background'>
+					<div style={{ margin: '5% 15%', height: '80%', borderRadius: '15px', backgroundColor: 'rgba(255, 255, 255, 0.95)' }}>
+						<div style={{ height: '100%', borderRadius: '15px' }}>
+							<div style={{ height: '100%', width: '5%', backgroundColor: '#e5e5e4', float: 'left', position: 'relative' }}>
+								<div style={{ padding: '10px' }}>
+									<Avatar size={42} src={img1} />
+								</div>
+								<div style={{ padding: '18px' }}>
+									<Avatar shape="square" size={25} src={img2} />
+								</div>
+								<div style={{ padding: '18px' }}>
+									<Avatar shape="square" size={25} src={img4} />
+								</div>
+								<div style={{ padding: '18px', position: 'absolute', left: '0', bottom: '0' }}>
+									<Avatar shape="square" size={25} src={img6} />
+								</div>
+							</div>
+							<div style={{ height: '100%', width: '22%', backgroundColor: '#f7f7f7', borderRight: 'solid 1px #e0e0e0', float: 'left' }}>
+								列表
+							</div>
+							<div style={{ height: '100%', width: '73%', backgroundColor: '#f3f3f3', float: 'right' }}>
+								<div style={{ borderBottom: 'solid 1px #e0e0e0', height: '8%' }}>
+									<div>会话名</div>
+								</div>
+								<div style={{ borderBottom: 'solid 1px #e0e0e0', height: '72%' }}>
+									<div>聊天记录区</div>
+								</div>
+								<div style={{ height: '20%' }}>
+									<div>输入区</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</Content>
+			</Layout>
 		)
 	}
 }
--- chat-client-web/src/containers/login/index.js
+++ chat-client-web/src/containers/login/index.js
@@ -1,28 +1,66 @@
-import React, { Component } from 'react'
-import { Layout, Row, Col, Image } from 'antd'
+import React, {Component} from 'react'
+import {Button, Form, Input, Layout} from 'antd'
 import './login.css'
-import img1 from '../../common/images/background01.jpg'
 
-const { Header, Content, Footer } = Layout;
+const { Header, Content } = Layout;
 
 class Login extends Component {
+
+	onFinish = (values) => {
+		console.log('Success:', values);
+		this.props.history.push('/home')
+	}
+
 	render() {
 		return (
 			<Layout style={{ height: '100vh' }}>
 				<Header className="P-L-header">
-					<div style={{ margin: '24px', font: 'italic 1.2em Georgia, serif' }}>
+					<div style={{ margin: '18px 150px', font: 'italic 1.8em Georgia, serif' }}>
 						TChat
 					</div>
 				</Header>
-				<Content style={{ margin: '30px 50px', backgroundColor: '#fff' }}>
-					<Row style={{ margin: '100px 100px', backgroundColor: '#4169E1' }}>
-						<Col span={12}>
-							<Image src={img1}/>
-						</Col>
-						<Col span={12}>
-							<div>表单</div>
-						</Col>
-					</Row>
+				<Content className='P-background'>
+					<div style={{ margin: '80px 200px', padding: '220px', backgroundColor: 'rgba(255, 255, 255, 0.95)' }}>
+						<div style={{ width: '70%' }}>
+							<Form
+								name="basic"
+								labelCol={{ span: 12 }}
+								wrapperCol={{ span: 12 }}
+								onFinish={this.onFinish}
+							>
+								<Form.Item label="昵称" name="username"
+									rules={[
+										{
+											required: true,
+											message: '请输入昵称',
+										},
+										{
+											min: 6,
+											message: '用户名不能少于 6 个字符'
+										}
+									]}
+								>
+									<Input />
+								</Form.Item>
+								<Form.Item label="密码" name="password"
+									rules={[
+										{
+											required: true,
+											message: '请输入密码',
+										},
+									]}
+								>
+									<Input.Password />
+								</Form.Item>
+								<Form.Item wrapperCol={{ offset: 12, span: 12 }}>
+									<Button type="primary" htmlType="submit">
+										登录
+									</Button>
+								</Form.Item>
+							</Form>
+						</div>
+					</div>
+
 				</Content>
 			</Layout>
 		)
