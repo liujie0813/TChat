438ce6b6e1da12ae4b5fac4fd91b69995a2b5e12
liujie0813 <18220747087@163.com>
2021-09-07 01:07:51 +0800

    temp commit

--- chat-client-web/src/components/chatPage/index.js
+++ chat-client-web/src/components/chatPage/index.js
@@ -1,31 +1,39 @@
 import React, {useState} from "react";
 import './chatPage.css'
-import { Image, Button } from 'antd'
+import { Image, Button, Input } from 'antd'
+import ReactWEditor from 'wangeditor-for-react';
 import {useSelector} from "react-redux";
 import img1 from '../../common/images/avator.png'
+
+const { TextArea } = Input;
 
 export default function ChatPage() {
 	const { activeMenu, activeChat, userInfo, page } = useSelector(state => state.user);
 	const type = page.type;
 	const data = page.data;
 
-	if (activeMenu.key === 'chatMenu' && type === 'chatPage') {
+	if (activeMenu === 'chatMenu' && type === 'chatPage') {
 		return (
 			<div style={{height: '100%', width: 'calc(100% - 370px)', backgroundColor: '#f3f3f3'}}>
-				<div style={{borderBottom: 'solid 1px #e0e0e0', height: '80px'}}>
-					<div>会话名</div>
+				<div style={{borderBottom: 'solid 1px #e0e0e0', height: '70px'}}>
+					<div style={{ fontSize: '20px', paddingTop: '25px', paddingLeft: '25px' }}>
+						{data.talkName}
+					</div>
 				</div>
 
 				<div style={{borderBottom: 'solid 1px #e0e0e0', height: 'calc((100% - 80px) * 0.7)'}}>
 					<div>聊天记录区</div>
 				</div>
 
-				<div style={{height: 'auto'}}>
-					<div>输入区</div>
+				<div style={{height: 'calc((100% - 70px) * 0.3)'}}>
+					<div style={{ }}>
+						
+					</div>
+
 				</div>
 			</div>
 		)
-	} else if (activeMenu.key === 'contactMenu' && type === 'contactInfoPage') {
+	} else if (activeMenu === 'contactMenu' && type === 'contactInfoPage') {
 		return (
 			<div style={{ marginTop: '140px', marginLeft: '90px', width: '400px', flexShrink: '1', display: 'flex', flexDirection: 'column' }}>
 				<div style={{ borderBottom: 'solid 1px #d0d0d0', display: 'flex', paddingBottom: '30px' }}>
